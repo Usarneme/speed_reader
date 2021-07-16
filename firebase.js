487cd9327b3ed3917b5daac7bb7feca6f3db1b58
Usarneme <jamestcoop@gmail.com>
2021-07-16 15:24:12 -0700

    :bug: attempted to get environment variables working with react native. tried react-native-env and react-native-config packages. neither working yet

--- firebase.js
+++ firebase.js
@@ -1,18 +1,26 @@
 import firebase from 'firebase/app';
-
 import 'firebase/database';
 import 'firebase/firestore';
 import 'firebase/auth';
+import {  FIREBASE_API_KEY,
+          FIREBASE_AUTH_DOMAIN,
+          FIREBASE_PROJECT_ID,
+          FIREBASE_STORAGE_BUCKET,
+          FIREBASE_MESSAGING_SENDER_ID,
+          FIREBASE_APP_ID,
+          FIREBASE_MEASUREMENT_ID, KEY, KAY, KOY } from '@env';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
+
+console.log('FIREBASE CHECK', FIREBASE_API_KEY, KEY, KAY, KOY)
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
