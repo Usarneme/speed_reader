e6fd04a57968bdc96b1617555d0068754b53f0dd
Usarneme <jamestcoop@gmail.com>
2021-07-27 14:50:07 -0700

    :tada: adds functionality for mobile users to select a file on their phone for upload to the speed reader; so far it collects the file name only

--- components/FileSelectMobile.js
+++ components/FileSelectMobile.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import * as FileSystem from 'expo-file-system'
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function FileSelectMobile(props) {
+  const [file, setFile] = useState('');
+
+  const getLocalFile = async () => {
+    console.log('get local file clicked')
+    const chosenFile = await DocumentPicker.getDocumentAsync()
+    if (!chosenFile || chosenFile.type !== 'success') {
+      console.log('no file chosen')
+    } else {
+      console.log('picked file:', chosenFile)
+      console.log(Object.keys(chosenFile))
+      setFile(chosenFile.uri)
+    }
+  }
+
   const styles = StyleSheet.create({
     container: {
       maxWidth: '100%',
@@ -18,6 +33,9 @@ export default function FileSelectMobile(props) {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Mobile file selector</Text>
+      <TouchableOpacity onPress={getLocalFile} >
+        <Text>Click here to select a text file to speed read</Text>
+      </TouchableOpacity>
     </View>
   )
 }
