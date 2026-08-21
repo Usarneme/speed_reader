ce49b2a1c07119193d9366477fd1d9d36a6dfdfa
Usarneme <jamestcoop@gmail.com>
2026-08-21 07:32:01 -0700

    :pushpin: adds metro config and eas client deps to align build versioning

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
   },
-};
+});
+
+module.exports = config;
